0f10aa724ecf29c6fa42a1f3dae4a6089fa2f97a
alexander nunez <alexandernunz@outlook.com>
2023-04-19 20:54:22 -0400

    Webpage skeleton and client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NavTabs from './components/NavTabs';
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Profile') {
+      return <Profile />;
+    }
+  }
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Header />
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <Footer/>
     </div>
   );
 }
-
 export default App;
---
+++ client/src/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Group from '../images/Group.png';
+
+export default function About() {
+    return (
+      <div class="idea">
+        <h1>Goals and Inspiration</h1>
+        <img src= {Group}  alt="Group photo" class ="center" width="200" height="250"></img>
+        <p>  Hello and Welcome to The Visible Hand. We are a dedicated and active forum pationate about navigating the trecherous Stock Market. Using our unique and inuitive tools found on the site users can better grasp and trade ideas regarding current stock market values. Our Team of passionate coders from The George Washington University Coding Bootcampy have made this site using inovative tools found in the competitive business enviroment. The goal of this website is to help fellow nerds eager to have a safe and inclusive forum where ideas and Stock Market hypothetical conclussions can be shared and conversed. 
+        </p>
+        <p>“I will tell you how to become rich. Close the doors. Be fearful when others are greedy. Be greedy when others are fearful.” By Warren Buffett</p>
+        <h2>Team Members</h2>
+        <p> Andre, Brian M, Survana Jadhav, Alexadner Nunez</p>
+        {/* <ul>
+                <a href ="https://github.com/braddahis">
+                    <li>
+                        Alexander Github
+                    </li>
+                </a>
+            </ul> */}
+      </div>
+    );
+  }
+  
