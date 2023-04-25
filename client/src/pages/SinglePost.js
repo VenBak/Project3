01a739603883ab50462e3b7bf6784117e6e1e560
bpmcdonell <73715095+bpmcdonell@users.noreply.github.com>
2023-04-25 11:51:58 -0400

    set up single post visibility+started on comments

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/client';
 import { useMutation } from "@apollo/client";
 import { CREATE_COMMENT } from "../utils/mutations";
 import { QUERY_SINGLE_POST } from "../utils/queries";
+import CommentForm from "../components/CommentForm";
 
 
 import Auth from "../utils/auth";
@@ -21,11 +22,54 @@ const SinglePost = () => {
 
   const post = data?.post || {};
 
+  function hasComments() {
+    if (post.comments?.length) {
+      return (
+        <div>
+          {<CommentForm postId={post._id} />}
+          {post.comments.map((comment) => (
+            <div key={comment._id}>
+              <p>{comment.commentText}</p>
+              <p>{comment.commentAuthor}</p>
+              <p>{comment.createdAt}</p>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return <div>No comments yet!</div>;
+    }
+  }
+
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  return (true)
+  return (
+    <div className="singlePostPageContainer">
+      <div className="stockContainer">
+        <div className="stockDetails">
+          <h2>Stock Details TBD</h2>
+        </div>
+
+
+      </div>
+      <div className="postContainer">
+        <div className="postDetails">
+          <h2>{post.postTitle}</h2>
+          <p>{post.postText}</p>
+          <p>{post.postAuthor}</p>
+          <p>{post.createdAt}</p>
+        </div>
+        <div className="commentDetails">
+          <h2>Comments</h2>
+          <div>
+            {hasComments()}
+          </div>
+        </div>
+      </div>
+    </div>)
 }
 
 export default SinglePost;
