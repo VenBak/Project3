11b3ac2e63e329a71bf23349354f12418ae61efc
alexander nunez <alexandernunz@outlook.com>
2023-04-27 20:16:05 -0400

    Header changes

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -8,12 +8,12 @@ function NavTabs (){
     <ul className="nav justify-content-center Tabs">
       <li className="nav-item">
         <Link className="text-light" style={{textDecoration: "none"}} to="/home">
-          <h3 className="m-0 link">Home</h3>
+          <h3 className="m-0 link" style={{fontSize: "15px"}}>Home</h3>
         </Link>
       </li>
       <li className="nav-item">
       <Link className="text-light"style={{textDecoration: "none"}} to="/about">
-          <h3 className="m-0 link">About</h3>
+          <h3 style={{fontSize: "15px"}} className="m-0 link">About</h3>
         </Link>
       </li>
       {/* <li className="nav-item">
