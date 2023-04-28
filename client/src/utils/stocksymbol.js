ee8506673e10275d4edbcd34e498bb4ef35092eb
VenBak <andre.brahin@gmail.com>
2023-04-27 20:54:09 -0400

    added apik

--- client/src/utils/stocksymbol.js
+++ client/src/utils/stocksymbol.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Chart from 'chart.js/auto';
 
-const apiKey = ""; // put api key here
+const apiKey = "ME2E70D0E3N4FKCG"; // put api key here
 
 function findStockSymbol() {
   const companyName = document.getElementById("companyName").value.trim();
