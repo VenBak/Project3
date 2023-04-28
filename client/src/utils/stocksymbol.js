7a0a93a46f71f7e0a9f2be7bc84137a427d272c1
suvarna28 <jadhav.suvarna28@gmail.com>
2023-04-27 21:05:40 -0400

    Update stocksymbol.js

--- client/src/utils/stocksymbol.js
+++ client/src/utils/stocksymbol.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Chart from 'chart.js/auto';
 
-const apiKey = "AS7FA1OIMV12SMBZ"; 
+const apiKey = ""; 
 
 function findStockSymbol() {
   const companyName = document.getElementById("companyName").value.trim();
