9f1619a19e78959881b5aadcca65f931744e0a71
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-05-01 12:15:17 -0400

    Implemented pagination, update post and delete post

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useQuery } from '@apollo/client';
@@ -11,34 +11,72 @@ export default function Home() {
   const { loading, data } = useQuery(QUERY_POSTS);
   const posts = data?.posts || [];
 
+  const post_in_a_page = 6;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(posts.length / post_in_a_page);
+
+  function getCurrentPageItems() {
+    const startIndex = (currentPage - 1) * post_in_a_page;
+    const endIndex = startIndex + post_in_a_page;
+    return posts.slice(startIndex, endIndex);
+  }
+
+  function handleNextPage() {
+    setCurrentPage((currentPage) => currentPage + 1);
+  }
+
+  function handlePrevPage() {
+    setCurrentPage((currentPage) => currentPage - 1);
+  }
+
+  function handlePageClick(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
   return (
     <div className='home'>
-      <h1 style={{textAlign: "center"}}>Stock Forum</h1>
+      <h1 style={{ textAlign: "center" }}>Stock Forum</h1>
       {loading ? (
         <div>Loading...</div>
-      ) : (
-        <div style={{ display: "flex", justifyContent: "center", flexDirection: "column"}}> {
-          posts.map(post => {
-            return (
-              <Card key={post.id}>
-                <Card.Body>
-                  <Card.Title>{post.postTitle}</Card.Title>
-                  <Card.Text>
-                    {post.postText}
-                    {post.postAuthor}
-                  </Card.Text>
-                    <Link
-                      className="btn btn-primary btn-block"
-                    to={Auth.loggedIn() ? (`/post/${post._id}`) : (`/login`)}
-                    >
-                      Join the discussion on this post.
-                    </Link>
-                </Card.Body>
-              </Card>
-            )
-          })
-        } </div>
-      )}
+      ) : (getCurrentPageItems().map((post) => (
+            <Card key={post.id}>
+              <Card.Body>
+            <Card.Title>{post.postTitle} <span style={{ fontSize: 11 }}>By {post.postAuthor} On { post.createdAt}</span></Card.Title>
+                <Card.Text>{post.postText}</Card.Text>
+                {/* <Card.Text>{ post.createdAt} by {post.postAuthor}</Card.Text> */}
+                <Link
+                  className="btn btn-primary btn-block"
+                  to={Auth.loggedIn() ? (`/post/${post._id}`) : (`/login`)}
+                >
+                  Join the discussion on this post.
+                </Link>
+              </Card.Body>
+            </Card>
+          )))}
+      <div className='pages'>
+        <button class="btn"
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+          <button class="btn"
+            key={pageNumber}
+            onClick={() => handlePageClick(pageNumber)}
+            disabled={currentPage === pageNumber}
+          >
+            {pageNumber}
+          </button>
+        ))}
+        <button class="btn"
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { useMutation } from "@apollo/client";
-import { CREATE_COMMENT } from "../utils/mutations";
+import { useQuery, useMutation} from '@apollo/client';
+import { DELETE_POST } from "../utils/mutations";
 import { QUERY_SINGLE_POST } from "../utils/queries";
 import CommentForm from "../components/CommentForm";
 import CommentList from '../components/CommentList';
-
 
 import Auth from "../utils/auth";
 
@@ -21,6 +19,22 @@ const SinglePost = () => {
   });
 
   const post = data?.post || {};
+
+
+  const [deletePost, { error1, data1 }] = useMutation(DELETE_POST);
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    try {
+        const { data } = await deletePost({
+            variables: { postId: postId }
+        });
+        window.location.assign('/');
+    }
+    catch (e) {
+        console.error(e);
+    }
+};
 
   if (loading) {
     return <div>Loading...</div>;
@@ -38,6 +52,10 @@ const SinglePost = () => {
           <h2><b>{post.postTitle}</b></h2>
           <p>{post.createdAt} by {post.postAuthor}</p>
           <p>{post.postText}</p>
+        </div>
+        <div className="updatePost"> 
+          {Auth.getProfile().data.username === post.postAuthor ? <button type="submit" className="btn btn-primary" onClick={handleDelete}>Delete Post</button> : ''}
+          {Auth.getProfile().data.username === post.postAuthor ? <Link className="updatePostLink" to={(`/updatepost/${post._id}`)}>Update Post</Link> : ''}
         </div>
         <div className="commentDetails">
           <h2><b>Comments</b></h2>
